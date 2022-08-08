b55fe4c0f98bf46cf5c8c399b0f8028275d895c5
Felipe Batista Alves <felipe.batista@loft.com.br>
2022-08-08 17:11:31 -0300

    FEAT: auto create event-attached resources and relations

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const AWS = require('aws-sdk')
 const fs = require('fs')
 const logger = require('./utils/logger')
+const caseConverter = require('serverless-plantuml/src/utils/caseConverter')
 const template = require('./templates/template.json')
-const IS_DEBUG = process.env.SLS_DEBUG ? true : false
+const IS_DEBUG = Boolean(process.env.SLS_DEBUG)
 
 class PlantUml {
   constructor(serverless) {
     this.serverless = serverless
     this.hooks = {
-      initialize: async () => await this.initialize()
+      'before:offline:start': async () => await this.initialize(),
+      'before:deploy:deploy': async () => await this.initialize()
     }
   }
 
@@ -67,48 +69,107 @@ class PlantUml {
   }
 
   generateResources(service) {
-    let lambdas, resources
+    let lambdas, resources, relations
+    const functionNames = Object.keys(service.functions)
+    const resourceNames = Object.keys(service.resources.Resources)
+
     try {
-      lambdas = Object.keys(service.functions).reduce((acc, key) => {
-        const camelCaseName = service.serviceName
-          .replace(/-([a-z])/g, g => g[1].toUpperCase()) + key
-            .charAt(0).toUpperCase() + key.slice(1)
-        const kebabServiceName = service.serviceName.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
-        const lambda = `Lambda(${camelCaseName}, "${kebabServiceName}_${key}", "Lambda function")`
-        return acc + lambda + '\n'
+      lambdas = functionNames.reduce((acc, key) => {
+        const camelCaseName = caseConverter(service.serviceName, 'kebab', 'camel')
+          + caseConverter(key, 'camel', 'pascal')
+        const lambda = `Lambda(${camelCaseName}, "${service.serviceName}_${key}", "Lambda function")`
+        return `${acc}${lambda}\n`
       }, '')
     } catch (error) {
+      console.log(error);
       IS_DEBUG && logger(error, 'debug')
       logger('Could not process lambdas, skipping', 'warning')
     }
 
     try {
-      resources = Object.keys(service.resources.Resources).reduce((acc, key) => {
-        const resource = this.resourceTranslator(service.resources.Resources[key], service.stage)
-        return resource ? acc + resource + '\n' : acc
+      resources = resourceNames.reduce((acc, key) => {
+        const resource = this.resourceTranslator(service.resources.Resources[key], key, service.stage)
+        return resource ? `${acc}${resource}\n` : acc
       }, '')
+
+      const events = functionNames.reduce((acc, key) => {
+        const event = this.eventTranslator(service.functions[key].events[0])
+        return event ? `${acc}${event}\n` : acc
+      }, '')
+
+      const hasApiGateway = functionNames.some(key => Object.keys(service.functions[key].events[0])[0] === 'http')
+      if (hasApiGateway) {
+        const pascalCaseName = caseConverter(service.serviceName, 'kebab', 'pascal')
+        resources += `APIGateway(${service.stage}${pascalCaseName}, "${service.stage}-${service.serviceName}", "API Gateway")\n`
+      }
+      resources += events
     } catch (error) {
       IS_DEBUG && logger(error, 'debug')
       logger('Could not process resources, skipping', 'warning')
     }
-    return [lambdas, resources].filter(Boolean).join('\n')
+
+    try {
+      relations = functionNames.reduce((acc, key) => {
+        const lambda = caseConverter(service.serviceName, 'kebab', 'camel')
+          + caseConverter(key, 'camel', 'pascal')
+        const relation = this.relationTranslator(service.functions[key].events[0], lambda, service)
+        return relation ? `${acc}${relation}\n` : acc
+      }, '')
+    }
+    catch (error) {
+      IS_DEBUG && logger(error, 'debug')
+      logger('Could not process relations, skipping', 'warning')
+    }
+
+    return [lambdas, resources, relations].filter(Boolean).join('\n')
   }
 
-  resourceTranslator(resource, stage) {
+  relationTranslator(event, lambda, service) {
+    if (!event) return null
+    const { serviceName, stage } = service
+    const pascalCaseName = caseConverter(serviceName, 'kebab', 'pascal')
+    const pascalStage = caseConverter(stage, 'camel', 'pascal')
+    switch (Object.keys(event)[0]) {
+      case 'http':
+        return `Rel(${stage}${pascalCaseName}, ${lambda}, "${event.http.method.toUpperCase()} ${event.http.path}", "HTTP")`
+      case 'sqs':
+        const queueName = typeof event.sqs === 'string' ? 
+          caseConverter(event.sqs.split(':').pop(), 'kebab', 'camel') :
+          `${event.sqs.arn['Fn::GetAtt'][0]}${pascalStage}`
+        return `Rel(${queueName}, ${lambda}, "Subscriber", "SQS")`
+      default:
+        return null
+    }
+  }
+
+
+  eventTranslator(event) {
+    if (!event) return null
+    switch (Object.keys(event)[0]) {
+      case 'sqs':
+        if (typeof event.sqs !== 'string') return null
+        const queueName = event.sqs.split(':').pop()
+        const camelCaseName = caseConverter(queueName, 'kebab', 'camel')
+        return `SimpleQueueService(${camelCaseName}, "${queueName}", "SQS")`
+      case 'http':
+        return null
+      default:
+        IS_DEBUG && logger(`Event (${Object.keys(event)[0]}) not mapped, skipping`, 'debug')
+        return null
+    }
+  }
+
+  resourceTranslator(resource, key, stage) {
+    const pascalStage = caseConverter(stage, 'camel', 'pascal')
     const { Type, Properties } = resource
+    const resourceName = key + pascalStage
     switch (Type) {
       case 'AWS::DynamoDB::Table':
-        const tableName = Properties.TableName.replace(`-${stage}`, '')
-        const camelCaseTableName = tableName.replace(/-([a-z])/g, g => g[1].toUpperCase())
-        return `DynamoDB(${camelCaseTableName}, "${tableName}", "DynamoDB")`
+        return `DynamoDB(${resourceName}, "${Properties.TableName}", "DynamoDB")`
       case 'AWS::SNS::Topic':
-        const topicName = Properties.TopicName.replace(`-${stage}`, '')
-        const camelCaseTopicName = topicName.replace(/-([a-z])/g, g => g[1].toUpperCase())
-        return `SimpleNotificationService(${camelCaseTopicName}, "${topicName}", "SNS")`
+        return `SimpleNotificationService(${resourceName}, "${Properties.TopicName}", "SNS")`
       case 'AWS::SQS::Queue':
-        const queueName = Properties.QueueName.replace(`-${stage}`, '')
-        const camelCaseQueueName = queueName.replace(/-([a-z])/g, g => g[1].toUpperCase())
-        return `SimpleQueueService(${camelCaseQueueName}, "${queueName}", "SQS")`
+        return `SimpleQueueService(${resourceName}, "${Properties.QueueName}", "SQS")`
       default:
         IS_DEBUG && logger(`Resource (${Type}) not mapped, skipping`, 'debug')
         return null
