076c0d38301010fe6c3df1c6a09b8f92b57480ab
Felipe Batista Alves <felipe.batista@loft.com.br>
2022-08-24 15:04:05 -0300

    FIX: add error handling to resource and event builder and in the initialize method

--- src/index.js
+++ src/index.js
@@ -17,26 +17,33 @@ class PlantUml {
   }
 
   async initialize(deployToS3 = false) {
-    logger('Generating diagram...')
-    const { functions, custom, resources } = this.serverless.configurationInput
-    const serviceName = this.serverless.service.service
-    const stage = this.serverless.service.provider.stage
-    const options = {
-      path: custom?.plantUml?.path.replace(/^\/|\/$/g, '') || '',
-      name: custom?.plantUml?.name || serviceName,
-      s3Bucket: custom?.plantUml?.s3Bucket,
-      s3Path: custom?.plantUml?.s3Path?.replace(/^\/|\/$/g, '') || ''
-    }
+    try {
+      logger('Generating diagram...')
+      const { functions, custom, resources } = this.serverless.configurationInput
+      const serviceName = this.serverless.service.service
+      const stage = this.serverless.service.provider.stage
+      const options = {
+        path: custom?.plantUml?.path ?
+          custom.plantUml.path.replace(/^\/|\/$/g, '') : '',
+        name: custom?.plantUml?.name || serviceName,
+        s3Bucket: custom?.plantUml?.s3Bucket,
+        s3Path: custom.plantUml.s3Path ?
+          custom?.plantUml?.s3Path?.replace(/^\/|\/$/g, '') : ''
+      }
 
-    const items = this.generateResources({
-      serviceName,
-      functions,
-      resources,
-      stage
-    }, options)
-    const diagram = this.createDiagram(items, options)
-    this.saveDiagram(diagram, options)
-    if (deployToS3 && options.s3Bucket) await this.putS3(diagram, options)
+      const items = this.generateResources({
+        serviceName,
+        functions,
+        resources,
+        stage
+      }, options)
+      const diagram = this.createDiagram(items, options)
+      this.saveDiagram(diagram, options)
+      if (deployToS3 && options.s3Bucket) await this.putS3(diagram, options)
+    } catch (error) {
+      IS_DEBUG && console.error(error, '\n')
+      logger('Could not generate diagram', 'error')
+    }
   }
 
   createDiagram(items, options) {
@@ -78,15 +85,20 @@ class PlantUml {
       plantUmlService.resourceBuilder(service.functions[lambda], lambda, 'function')
     )
     const resources = resourceNames.map((resource) => {
-      const _resource = service.resources.Resources[resource]
-      if (!_resource) return
-      const resourceType = plantUmlService.getResourceType(_resource, 'resource')
-      const prefix = plantUmlService.getPrefix(resourceType, resourceType)
-      const _resourceName = plantUmlService
-        .getItemLabel(_resource, prefix, resource)
-        .replace(`-${service.stage}`, '')
-      const resourceName = caseConverter(_resourceName, 'kebab', 'camel')
-      return plantUmlService.resourceBuilder(_resource, resourceName, 'resource')
+      try {
+        const _resource = service.resources.Resources[resource]
+        if (!_resource) return
+        const resourceType = plantUmlService.getResourceType(_resource, 'resource')
+        const prefix = plantUmlService.getPrefix(resourceType, resourceType)
+        const _resourceName = plantUmlService
+          .getItemLabel(_resource, prefix, resource)
+          .replace(`-${service.stage}`, '')
+        const resourceName = caseConverter(_resourceName, 'kebab', 'camel')
+        return plantUmlService.resourceBuilder(_resource, resourceName, 'resource')
+      } catch (error) {
+        IS_DEBUG && console.error(error, '\n')
+        logger(`Could not build resource ${resource}`, 'warning')
+      }
     })
     const events = functionNames.map((lambda) => {
       const event = service.functions[lambda]?.events[0]
@@ -96,32 +108,37 @@ class PlantUml {
         return plantUmlService.resourceBuilder(event, 'http', 'event')
       if (typeof event[key] === 'string' || typeof event[key]?.arn === 'string') {
         const arn = event[key]?.arn || event[key]
-        const eventName = arn.split(':').pop().split('-')[0]         
+        const eventName = arn.split(':').pop().split('-')[0]
         return plantUmlService.resourceBuilder(event, eventName, 'event')
       }
     })
     const eventRelations = functionNames.map((lambda) => {
-      const event = service.functions[lambda]?.events[0]
-      if (!event) return
-      const key = Object.keys(event)[0]
-      let eventName = ''
-      if (key === 'http')
-        return plantUmlService.relationBuilder(event, 'http', lambda, 'events')
-      if (typeof event[key] === 'string' || typeof event[key]?.arn === 'string') {
-        const arn = event[key]?.arn || event[key]
-        eventName = arn.split(':').pop().split('-')[0]
+      try {
+        const event = service.functions[lambda]?.events[0]
+        if (!event) return
+        const key = Object.keys(event)[0]
+        let eventName = ''
+        if (key === 'http')
+          return plantUmlService.relationBuilder(event, 'http', lambda, 'events')
+        if (typeof event[key] === 'string' || typeof event[key]?.arn === 'string') {
+          const arn = event[key]?.arn || event[key]
+          eventName = arn.split(':').pop().split('-')[0]
+          return plantUmlService.relationBuilder(event, eventName, lambda, 'events')
+        }
+        if (!service?.resources?.Resources) return
+        const resourceName = event[key]?.arn['Fn::GetAtt'][0]
+        const resource = service.resources.Resources[resourceName]
+        const resourceType = plantUmlService.getResourceType(resource, 'resource')
+        const prefix = plantUmlService.getPrefix(resourceType, resourceType)
+        const _eventName = plantUmlService
+          .getItemLabel(resource, prefix, resourceName)
+          .replace(`-${service.stage}`, '')
+        eventName = caseConverter(_eventName, 'kebab', 'camel')
         return plantUmlService.relationBuilder(event, eventName, lambda, 'events')
+      } catch (error) {
+        IS_DEBUG && console.error(error, '\n')
+        logger(`Could not build relation for lambda ${lambda}`, 'warning')
       }
-      if(!service?.resources?.Resources) return
-      const resourceName = event[key]?.arn['Fn::GetAtt'][0]
-      const resource = service.resources.Resources[resourceName]
-      const resourceType = plantUmlService.getResourceType(resource, 'resource')
-      const prefix = plantUmlService.getPrefix(resourceType, resourceType)
-      const _eventName = plantUmlService
-        .getItemLabel(resource, prefix, resourceName)
-        .replace(`-${service.stage}`, '')
-      eventName = caseConverter(_eventName, 'kebab', 'camel')
-      return plantUmlService.relationBuilder(event, eventName, lambda, 'events')
     })
 
     const items = _.concat(lambdas, resources, events, eventRelations)
