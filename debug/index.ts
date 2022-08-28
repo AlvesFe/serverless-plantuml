faceea776f0aa534d698a177a195225a9e6c02ee
Felipe Batista Alves <felipe.alves135@etec.sp.gov.br>
2022-08-27 23:09:03 -0300

    FEAT: add a debug file to facilitate development

---
+++ debug/index.ts
@@ -0,0 +1,12 @@
+process.env.SLS_DEBUG = '*'
+import * as fs from 'fs'
+import PlantUml from '../src'
+import minimist from 'minimist'
+const argv = minimist(process.argv.slice(2))
+
+const upload = (argv.u || argv.upload) ?? false
+const serverlessFile = fs.readFileSync('./debug/data/serverlessFile.json').toString()
+const payload = JSON.parse(serverlessFile)
+const puml = new PlantUml(payload)
+
+puml.initialize(upload).then(() => console.log('EXECUTED'))
